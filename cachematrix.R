8f6e038b93d58aaef159c3be8c3f1ae4d3c59f8a
mshayduk <mshayduk@gmail.com>
2015-12-17 18:58:13 +0100

    indentation changed to 4 spaces

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,36 @@
-  ## This script provides functions to construct and access the special square matrix object
-  ## able to cache it's inverse to avoid potentially time-consuming inverse re-calculations 
+## This script provides functions to construct and access the special square
+## matrix object able to cache it's inverse to avoid potentially 
+## time-consuming inverse re-calculations 
 
-  # obj <- makeCacheMatrix(x = matrix())  constructs object from square matrix x that can hold x's inverse
-  # list of methods: set(matrix), get(), setinverse(matrix), getinverse()
-  makeCacheMatrix <- function(x = matrix()) {
+# obj <- makeCacheMatrix(x = matrix())  constructs object from 
+# square matrix x that can hold x's inverse
+# list of methods: set(matrix), get(), setinverse(matrix), getinverse()
+makeCacheMatrix <- function(x = matrix()) {
     if (!is.matrix(x) | (is.matrix(x) & (dim(x)[1]!=dim(x)[2])))
-      stop("argument is not a square matrix")
+        stop("argument is not a square matrix")
     
     inv <- NULL
     set <- function(y) {
-      x <<- y
-      inv <<- NULL
+        x <<- y
+        inv <<- NULL
     }
     get <- function() x
     setinverse <- function(inverseOfx) inv <<- inverseOfx
     getinverse <- function() inv
     list(set = set, get = get,
-      setinverse = setinverse,
-      getinverse = getinverse)
-  }
+         setinverse = setinverse,
+         getinverse = getinverse)
+}
 
-  # cacheSolve(x,...)  returns the inverse of the square matrix in object x
-  # it returns cached inverse if it is present in x 
-  cacheSolve <- function(x, ...) {
+# cacheSolve(x,...)  returns the inverse of the square matrix in object x
+# it returns cached inverse if it is present in x 
+cacheSolve <- function(x, ...) {
     # trying to get the cached inverse of the data in x
     inv <- x$getinverse()
-    # if it exsists/NOT NULL, returning it, avoiding re-calculation
+    # if it exsists/NOT NULL, returning it, avoiding re-calculatiogn
     if(!is.null(inv)) {
-      message("getting cached data")
-      return(inv)
+        message("getting cached data")
+        return(inv)
     }
     # if the cached inverse is not existing, calculating and caching it in x
     data <- x$get()
